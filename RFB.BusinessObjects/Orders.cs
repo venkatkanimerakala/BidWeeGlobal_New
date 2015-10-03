6be1fe77e2c6d77687b4163d6cf49e771b1de910
kuladeep18 <kuladeep.mohan@gmail.com>
2015-10-03 19:48:11 +0530

    added orders properties

--- RFB.BusinessObjects/Orders.cs
+++ RFB.BusinessObjects/Orders.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RFB.BusinessObjects
+namespace RFB.BusinessModel
 {
   public class Orders
     {
+      public Int64 _orderId;
+      public Int64 _productId;
+      public Int64 _quantity;
+      public decimal _purchasePrice;
+      public DateTime _orderDate;
+      public string _orderUniqueId;
+      public DateTime _deliveryDate;
+      public bool _isSuccessful;
+      public bool _isDelivered;
+      public Int64 _sellerId;
+      public Int64 _customerId;
+      public Int64 _currencyId;
+      public Int64 _rfbID;
+
     }
 }
